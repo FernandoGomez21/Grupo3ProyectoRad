7c4c5e969f2633aa6abf6db8e567c993de358ab9
PC <PC@DESKTOP-TR3LV0G>
2024-04-15 22:00:10 -0600

    Se agrego caapa de negocio de factura detalle, tambien la capa de datos y la de presentacion

---
+++ Datos/DFacturaDetalle.cs
@@ -0,0 +1,64 @@
+﻿using Datos.BaseDatos.Models;
+using Datos.Core;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class DFacturaDetalle
+    {
+        private UnitOfWork _unitOfWork;
+
+        public DFacturaDetalle()
+        {
+            _unitOfWork = new UnitOfWork();
+        }
+
+        public List<FacturaDetalle> ObtenerTodosLosDetalles()
+        {
+            return _unitOfWork.Repository<FacturaDetalle>()
+                .Consulta()
+                .Include(fd => fd.Factura)
+                .Include(fd => fd.Pedidos)
+                .ToList();
+        }
+
+        public int Agregar(FacturaDetalle detalle)
+        {
+            _unitOfWork.Repository<FacturaDetalle>().Agregar(detalle);
+            return _unitOfWork.Guardar();
+        }
+
+        public int Editar(FacturaDetalle detalle)
+        {
+            var detalleInDb = _unitOfWork.Repository<FacturaDetalle>().Consulta().FirstOrDefault(fd => fd.FacturaDetalleId == detalle.FacturaDetalleId);
+            if (detalleInDb != null)
+            {
+                detalleInDb.FacturaId = detalle.FacturaId;
+                detalleInDb.ProductosId = detalle.ProductosId;
+                detalleInDb.Precio = detalle.Precio;
+                detalleInDb.Total = detalle.Total;
+                detalleInDb.SubTotal = detalle.SubTotal;
+                detalleInDb.Descuento = detalle.Descuento;
+                _unitOfWork.Repository<FacturaDetalle>().Editar(detalleInDb);
+                return _unitOfWork.Guardar();
+            }
+            return 0;
+        }
+
+        public int Eliminar(int facturaDetalleId)
+        {
+            var detalleInDb = _unitOfWork.Repository<FacturaDetalle>().Consulta().FirstOrDefault(fd => fd.FacturaDetalleId == facturaDetalleId);
+            if (detalleInDb != null)
+            {
+                _unitOfWork.Repository<FacturaDetalle>().Eliminar(detalleInDb);
+                return _unitOfWork.Guardar();
+            }
+            return 0;
+        }
+    }
+}
---
+++ Grupo3ProyectoRad/FrmDetalleFactura.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Grupo3ProyectoRad
+{
+    partial class FrmDetalleFactura
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label9 = new System.Windows.Forms.Label();
+            this.dgvproducto = new System.Windows.Forms.DataGridView();
+            this.BtnConsultar = new System.Windows.Forms.Button();
+            this.DtpIn = new System.Windows.Forms.DateTimePicker();
+            this.DtpFin = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtSumaTotal = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvproducto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(197, 9);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(317, 17);
+            this.label9.TabIndex = 110;
+            this.label9.Text = "REPORTES Y DETALLES DE LA FACTURA";
+            // 
+            // dgvproducto
+            // 
+            this.dgvproducto.AllowUserToOrderColumns = true;
+            this.dgvproducto.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvproducto.BackgroundColor = System.Drawing.SystemColors.ControlLight;
+            this.dgvproducto.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvproducto.Location = new System.Drawing.Point(78, 104);
+            this.dgvproducto.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvproducto.Name = "dgvproducto";
+            this.dgvproducto.RowHeadersWidth = 51;
+            this.dgvproducto.RowTemplate.Height = 24;
+            this.dgvproducto.Size = new System.Drawing.Size(710, 342);
+            this.dgvproducto.TabIndex = 101;
+            // 
+            // BtnConsultar
+            // 
+            this.BtnConsultar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.BtnConsultar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnConsultar.Image = global::Grupo3ProyectoRad.Properties.Resources.guardar;
+            this.BtnConsultar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnConsultar.Location = new System.Drawing.Point(556, 9);
+            this.BtnConsultar.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnConsultar.Name = "BtnConsultar";
+            this.BtnConsultar.Size = new System.Drawing.Size(116, 31);
+            this.BtnConsultar.TabIndex = 103;
+            this.BtnConsultar.Text = "Consultar";
+            this.BtnConsultar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BtnConsultar.UseVisualStyleBackColor = false;
+            this.BtnConsultar.Click += new System.EventHandler(this.BtnConsultar_Click);
+            // 
+            // DtpIn
+            // 
+            this.DtpIn.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.DtpIn.Location = new System.Drawing.Point(197, 72);
+            this.DtpIn.Name = "DtpIn";
+            this.DtpIn.Size = new System.Drawing.Size(200, 20);
+            this.DtpIn.TabIndex = 111;
+            // 
+            // DtpFin
+            // 
+            this.DtpFin.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.DtpFin.Location = new System.Drawing.Point(508, 72);
+            this.DtpFin.Name = "DtpFin";
+            this.DtpFin.Size = new System.Drawing.Size(200, 20);
+            this.DtpFin.TabIndex = 112;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(93, 72);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 17);
+            this.label1.TabIndex = 113;
+            this.label1.Text = "Fecha Inicial";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(411, 72);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 17);
+            this.label2.TabIndex = 114;
+            this.label2.Text = "Fecha Final";
+            // 
+            // TxtSumaTotal
+            // 
+            this.TxtSumaTotal.Location = new System.Drawing.Point(607, 452);
+            this.TxtSumaTotal.Name = "TxtSumaTotal";
+            this.TxtSumaTotal.Size = new System.Drawing.Size(181, 20);
+            this.TxtSumaTotal.TabIndex = 115;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(517, 455);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 17);
+            this.label3.TabIndex = 116;
+            this.label3.Text = "SumaTotal";
+            // 
+            // FrmDetalleFactura
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 475);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtSumaTotal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DtpFin);
+            this.Controls.Add(this.DtpIn);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.BtnConsultar);
+            this.Controls.Add(this.dgvproducto);
+            this.Name = "FrmDetalleFactura";
+            this.Text = "FrmDetalleFactura";
+            this.Load += new System.EventHandler(this.FrmDetalleFactura_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvproducto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button BtnConsultar;
+        private System.Windows.Forms.DataGridView dgvproducto;
+        private System.Windows.Forms.DateTimePicker DtpIn;
+        private System.Windows.Forms.DateTimePicker DtpFin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtSumaTotal;
+        private System.Windows.Forms.Label label3;
+    }
+}
