a8164f922068bec2dd4447027b2a86b06db3862b
ferna <ferna@FERNANDOGOMEZ>
2024-04-16 19:47:51 -0600

    Correccion de Errores

--- Grupo3ProyectoRad/FrmDetalleFactura.cs
+++ Grupo3ProyectoRad/FrmDetalleFactura.cs
@@ -1,4 +1,5 @@
-﻿using Datos.BaseDatos.Models;
+﻿using Datos;
+using Datos.BaseDatos.Models;
 using Negocio;
 using System;
 using System.Collections.Generic;
@@ -12,28 +13,65 @@ using System.Windows.Forms;
 
 namespace Grupo3ProyectoRad
 {
-    public partial class FrmDetalleFactura : Form
+    public partial class FrmDetalleFactura : MenuPrin
     {
 
         string fechain, fechafin;
 
-        private NFacturaDetalle nFacturaDetalle; 
-
+        private NFacturaDetalle nFacturaDetalle;
+        private NFactura nFactura;
         public FrmDetalleFactura()
         {
             InitializeComponent();
             nFacturaDetalle = new NFacturaDetalle();
-        }
+             nFactura = new NFactura();
+    }
 
         private void setvalue()
         {
             fechain = DtpIn.Text;
             fechafin = DtpFin.Text;
         }
+        private void CargarDatos()
+        {
+            var clientes = nFactura.FacturaInactivas().Select(c => new { c.FacturaId, c.ClienteId, c.Cliente.Nombres, c.Cliente.Apellidos, c.Pedido.PedidoId, c.FechaFactura, c.Descuento, c.SubTotal, c.Total });
+            dgvproducto.DataSource = clientes.ToList();
 
+        }
         private void FrmDetalleFactura_Load(object sender, EventArgs e)
         {
+            CargarDatos();
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvproducto_RowHeaderMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            Factura factura = new Factura()
+            {
+                PedidoId = int.Parse(dgvproducto.CurrentRow.Cells["PedidoId"].Value.ToString()),
+                ClienteId = int.Parse(dgvproducto.CurrentRow.Cells["ClienteId"].Value.ToString()),
+                FechaCreacion = DateTime.Now,
+                FechaFactura = DateTime.Parse(dgvproducto.CurrentRow.Cells["FechaFactura"].Value.ToString()),
+                Estado = true,
+                Total = decimal.Parse(dgvproducto.CurrentRow.Cells["Total"].Value.ToString()),
+                SubTotal = decimal.Parse(dgvproducto.CurrentRow.Cells["SubTotal"].Value.ToString()),
+                Descuento = decimal.Parse(dgvproducto.CurrentRow.Cells["Descuento"].Value.ToString()),
+            };
+            if (!string.IsNullOrEmpty(dgvproducto.CurrentRow.Cells["FacturaId"].Value.ToString()) || !string.IsNullOrWhiteSpace(dgvproducto.CurrentRow.Cells["FacturaId"].Value.ToString()))
+            {
+                if (int.Parse(dgvproducto.CurrentRow.Cells["FacturaId"].Value.ToString()) != 0)
+                {
+                    factura.FacturaId = int.Parse(dgvproducto.CurrentRow.Cells["FacturaId"].Value.ToString());
+                }
+            }
+            nFactura.AgregarFactura(factura);
+            CargarDatos();
+            MessageBox.Show("Factura Realizada");
         }
 
         private void BtnConsultar_Click(object sender, EventArgs e)
